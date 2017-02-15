e9c335c97f733fdebca4bce40ce51f4d5a8f6daa
Learnallthetime <philipboffa@gmail.com>
2017-02-15 17:34:19 -0600

    Got rid of d1

--- src/dAndD/Main.java
+++ src/dAndD/Main.java
@@ -1,10 +1,54 @@
 package dAndD;
 
-public class Main {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Main {	
+	public static int optionsList(String title, String[] options)
+	{
+		Scanner kbd = new Scanner(System.in);
+		int result = 0;
+
+		do
+		{
+			System.out.println(title);
+			for (int i = 0; i < options.length; i++)
+				System.out.println(i+1+ ". " + options[i]);
+
+			String input = kbd.nextLine();
+			try
+			{
+				result = Integer.parseInt(input);
+			}
+			catch (NumberFormatException ex)
+			{
+			}
+		}
+		while (result < 1 || result > options.length);
+
+			return result;
+	}
+
+
+
+	public static void main(String [] args){
+
+		String[] yesNo = {"Yes", "No"};
+		int answer = optionsList("Are you to play ready?", yesNo);
+		if (answer == 2)
+		{
+			System.out.println("Come back when your ready to be challenged!");
+			return;
+		}
+		
+		System.out.println("Welcome to the D&D, may the Gods be with you...");	
+		System.out.println("Choose your character build"); 
+		
+				 
 
 	}
+	
+	
+	
+	
 
 }
